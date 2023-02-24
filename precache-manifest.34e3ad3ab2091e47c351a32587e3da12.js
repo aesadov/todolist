b41be0a3d5bcef4d845ad02dcc77b278a0869964
aesadov <aesadov@gmail.com>
2023-02-24 19:12:48 +0300

    Updates

---
+++ precache-manifest.34e3ad3ab2091e47c351a32587e3da12.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c06a492b6e432a76ad628959dcd65077",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "d858bf47492997eedfb2",
+    "url": "/todolist/static/css/main.155097fa.chunk.css"
+  },
+  {
+    "revision": "f9e1773707c838da6359",
+    "url": "/todolist/static/js/2.11b78d41.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/todolist/static/js/2.11b78d41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d858bf47492997eedfb2",
+    "url": "/todolist/static/js/main.5a01ec60.chunk.js"
+  },
+  {
+    "revision": "471e54e937a3aef18e16",
+    "url": "/todolist/static/js/runtime-main.283d8678.js"
+  }
+]);
